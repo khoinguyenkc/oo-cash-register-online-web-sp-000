38fa45cb695aedc994ea3785bd819f5d377642ac
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-19 11:06:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,8 +9,9 @@ end
 
 def add_item(title, price, quantity = 1)
   @total += (price * quantity) #* (@discount / 100)
-  quantity.times do  
-  @items << title #they want repeats. 
+  @lasttransactionamount = price
+  quantity.times do
+  @items << title #they want repeats.
   end
 end
 
@@ -31,6 +32,16 @@ end
 def items
   @items
 end
+
+def void_last_transaction
+  #voids the last item. even if u add 3 tomatoes at a time with add_item, it only removes one tomato
+  #also it doesnt refund any discount amount you may have.. highly unrealistic
+  #this makes use of a variable that keeps track of the last transaction's amount
+  #ideally we keep track of ALL amounts but this is a simple cash register
+  @items.pop
+  @total -= @lasttransactionamount
+end
+
 
 end
 
