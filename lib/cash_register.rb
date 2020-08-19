abd807725195dc0dd8e9c8bc8df93d6272254d13
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-19 10:33:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,6 +3,12 @@ attr_accessor :discount, :total
 def initialize(discount = 0)
   @total = 0
   @discount = discount
+  @items = []
+end
+
+def add_item(title, price, quantity = 1)
+  @items << title
+  @total += (price * quantity) * (@discount / 100)
 end
 
 
